ba6e42052d25556f6c8e38b2bffc4a1b9ed400aa
Mikhail Shamin <schennoy@yandex.ru>
2018-12-17 00:05:32 +0300

    Release - Added slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import styled from 'styled-components';
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+import styled from 'styled-components'
+
+import Slider from './Slider'
 
 const Repair = styled.div`
   color: #ffffff;
@@ -49,8 +51,8 @@ class Main extends React.Component{
 						<CallBtn>Отправить заявку!</CallBtn>
 					</Col>
 					<Col lg={6} lgOffset={1}>
-					
-					</Col>
+              <Slider />
+  				</Col>
 				</Row>
 			)
 	}
